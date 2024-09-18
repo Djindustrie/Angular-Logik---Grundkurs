dab5ed3fc7b41d5aad8ab454b985515586b352d5
Djindustrie <149553624+Djindustrie@users.noreply.github.com>
2024-09-18 11:23:12 +0200

    first commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { FrutitlistComponent } from './frutitlist/frutitlist.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: FrutitlistComponent},
+];
